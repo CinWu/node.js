4dbcc5f24d15024a9955bd45cc9895812bf60ba6
Cindy <cindw@hotmail.com>
2015-04-19 21:02:05 -0400

    update

--- read.js
+++ read.js
@@ -1,9 +1,19 @@
 var http = require('http');
 var fileSystem = require('fs');
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+var userCount = 0;
 
+server = http.createServer();
+
+server.on('request', function (request, response) {
+    console.log('New connection');
+    userCount++;
+
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello!\n');
+    response.write('We have had '+userCount+' visits!\n\n');
+
+    response.write('Reading files: \n');
     var read_stream = fileSystem.createReadStream('myfile.txt');
     read_stream.on('data', writeCallback);
     read_stream.on('close', closeCallback);
