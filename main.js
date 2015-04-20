e3711680e4aff73e8168be8b26c2d05c9ab81f88
Cindy <cindw@hotmail.com>
2015-04-19 21:00:21 -0400

    update

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-var moduletest = require('./moduletest');
-moduletest.exportfunc();
+var FoodList = require("./moduletest");
+var foods = new FoodList();
+ 
+foods.on("added", function (foods) {
+    console.log('Added '+foods.count+" "+foods.name+'(s).' );
+});
+ 
+foods.add({ name: "pie", count: 20 });
--- moduletest.js
+++ moduletest.js
@@ -1,4 +1,33 @@
-//an 'exports' object is available in every module, and it is returned whenever the require function is used to include the module
-exports.exportfunc = function() {
-  console.log("I am a module. This function is in a separate file.");
+var util = require("util");
+var EventEmitter = require("events").EventEmitter;
+
+var id = 1;
+var db = {
+    foods: [
+        { id: id++, name: "doughnut", count: 5    },
+        { id: id++, name: "cookie", count: 10 },
+        { id: id++, name: "cake", count: 15     }
+    ]
+};
+
+function FoodList () {
+    EventEmitter.call(this);
 }
+
+//Inherits EventEmitter.prototype
+util.inherits(FoodList, EventEmitter);
+
+FoodList.prototype.add = function(item) {
+    item.id = id++;
+    db.foods.push(item);
+    this.emit("added", item);  
+};
+
+FoodList.prototype.all = function() {
+    return db.foods;
+};
+
+//an 'exports' object is available in every module, 
+//and it is returned whenever the require function is used to include the modul 
+module.exports = FoodList;
+
